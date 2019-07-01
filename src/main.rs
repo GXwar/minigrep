919d5dfaafa135a5873939d13c34272e47167f40
GXwar <richardxulh@gmail.com>
2019-07-01 00:11:57 -0700

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 struct Config {
     query: String,
@@ -7,24 +9,36 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
         
-        Config {
+        Ok(Config {
             query,
             filename
-        }
+        })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("\nWith text:\n{}", contents);
+    Ok(())
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for \"{}\" In file \"{}\"", config.query, config.filename);
-
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-    println!("\nWith text:\n{}", contents);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
