cbd9e8a055bb4f8cf97fb6c16a029425b05bc156
GXwar <richardxulh@gmail.com>
2019-06-30 23:45:03 -0700

    extract arguments and grouping configuration values

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
 use std::env;
+use std::fs;
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        
+        Config {
+            query,
+            filename
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for \"{}\" In file \"{}\"", config.query, config.filename);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+    println!("\nWith text:\n{}", contents);
 }
